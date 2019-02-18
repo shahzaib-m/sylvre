bf9237ee39404065d5bd8a04809c1ef8adc1eab8
Shahzaib Mirani <shahzaib6874@gmail.com>
2019-02-18 19:42:16 +0000

    Remove CLSCompliant attributes (VS warnings)

--- src/Sylvre.Core/Grammar/ANTLR4Generated/SylvreParser.cs
+++ src/Sylvre.Core/Grammar/ANTLR4Generated/SylvreParser.cs
@@ -31,7 +31,7 @@ using Antlr4.Runtime.Tree;
 using DFA = Antlr4.Runtime.Dfa.DFA;
 
 [System.CodeDom.Compiler.GeneratedCode("ANTLR", "4.7.2")]
-[System.CLSCompliant(false)]
+//[System.CLSCompliant(false)]
 public partial class SylvreParser : Parser {
 	protected static DFA[] decisionToDFA;
 	protected static PredictionContextCache sharedContextCache = new PredictionContextCache();
