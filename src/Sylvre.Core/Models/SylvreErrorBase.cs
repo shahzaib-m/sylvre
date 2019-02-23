d66eb4b79b00b96ff26278c8674d5042f3dafe6c
Shahzaib Mirani <shahzaib6874@gmail.com>
2019-02-23 01:10:39 +0000

    Refactor Sylvre error

---
+++ src/Sylvre.Core/Models/SylvreErrorBase.cs
@@ -0,0 +1,25 @@
+﻿namespace Sylvre.Core.Models
+{
+    /// <summary>
+    /// The base class representing a Sylvre error.
+    /// </summary>
+    public abstract class SylvreErrorBase
+    {
+        /// <summary>
+        /// The exact symbol that caused the error.
+        /// </summary>
+        public string Symbol { get; set; }
+        /// <summary>
+        /// The line number of the error in the original input.
+        /// </summary>
+        public int Line { get; set; }
+        /// <summary>
+        /// The character position in the line number of the error in the original input.
+        /// </summary>
+        public int CharPositionInLine { get; set; }
+        /// <summary>
+        /// The error message detailing the parse error.
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
