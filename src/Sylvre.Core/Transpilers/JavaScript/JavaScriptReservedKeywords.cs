4c6c35a8e2a047aeac970d28f58cdc426871708e
Shahzaib Mirani <shahzaib6874@gmail.com>
2019-02-23 17:37:21 +0000

    Implement reserved JavaScript keywords class

---
+++ src/Sylvre.Core/Transpilers/JavaScript/JavaScriptReservedKeywords.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Sylvre.Core.Transpilers.JavaScript
+{
+    /// <summary>
+    /// A class for reserved keywords in JavaScript. Based on https://www.w3schools.com/js/js_reserved.asp.
+    /// </summary>
+    public static class JavaScriptReservedKeywords
+    {
+        /// <summary>
+        /// The HashSet containing all the reserved keywords in JavaScript.
+        /// </summary>
+        private static readonly HashSet<string> _reservedKeywords = new HashSet<string>();
+
+        /// <summary>
+        /// Static constructor to initialise _reservedKeywords with the keywords in JavaScriptReservedKeywords.txt.
+        /// </summary>
+        static JavaScriptReservedKeywords()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Sylvre.Core.Transpilers.JavaScript.JavaScriptReservedKeywords.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string keyword;
+                while ((keyword = reader.ReadLine()) != null)
+                {
+                    _reservedKeywords.Add(keyword);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks to see if the given input is a reserved JavaScript keyword.
+        /// </summary>
+        /// <param name="input">THe input string to check.</param>
+        /// <returns>True if input is a reserved JavaScript keyword, false otherwise.</returns>
+        public static bool IsReservedKeyword(string input)
+        {
+            return _reservedKeywords.Contains(input);
+        }
+    }
+}
