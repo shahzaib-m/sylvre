798f8498d556336f872a5b954e08a44b4ea8e74a
Shahzaib Mirani <shahzaib6874@gmail.com>
2019-02-23 02:36:07 +0000

    Implement function block transpiler (+tests)

--- src/Sylvre.Core/Transpilers/JavaScript/BlockVisitors.cs
+++ src/Sylvre.Core/Transpilers/JavaScript/BlockVisitors.cs
@@ -12,7 +12,7 @@ namespace Sylvre.Core.Transpilers.JavaScript
         {
             if (context.function_block() != null)
             {
-                throw new NotImplementedException();
+                VisitFunction_block(context.function_block());
             }
             else if (context.if_block() != null)
             {
@@ -30,6 +30,39 @@ namespace Sylvre.Core.Transpilers.JavaScript
             {
                 throw new NotImplementedException();
             }
+
+            return null;
+        }
+
+        public override object VisitFunction_block([NotNull] Function_blockContext context)
+        {
+            _output.Append("function ")
+                   .Append(context.variable_reference().GetText())
+                   .Append('(');
+
+            if (context.parameters() != null)
+            {
+                var variableReferences = context.parameters().variable_reference();
+                for (int i = 0; i < variableReferences.Length; i++)
+                {
+                    _output.Append(variableReferences[i].GetText());
+
+                    if (i != variableReferences.Length - 1)
+                    {
+                        _output.Append(',');
+                    }
+                }
+            }
+
+            _output.Append(')')
+                   .Append('{');
+
+            foreach (var nestableBlock in context.nestable_block())
+            {
+                VisitNestable_block(nestableBlock);
+            }
+
+            _output.Append('}');
 
             return null;
         }
---
+++ src/Sylvre.Tests/Core/TranspilerTests/JavaScript/FunctionDeclarationTests.cs
@@ -0,0 +1,43 @@
+﻿using Sylvre.Core;
+using Sylvre.Core.Models;
+
+using NUnit.Framework;
+
+namespace Sylvre.Tests.Core.TranspilerTests.JavaScript
+{
+    class FunctionDeclarationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(
+            "function quickSort PARAMS num_array, low, high < >",
+           @"""use strict"";[\n ]*function[\n ]+quickSort\(num_array,[\n ]*low,[\n ]*high\)[\n ]*{[\n ]*}")]
+        [TestCase(
+            "function quickSort < >",
+           @"""use strict"";[\n ]*function[\n ]+quickSort\(\)[\n ]*{[\n ]*}")]
+        [TestCase(
+            "function quickSort PARAMS single < >",
+           @"""use strict"";[\n ]*function[\n ]+quickSort\(single\)[\n ]*{[\n ]*}")]
+        public void Should_Output_Valid_JavaScript_Function(string sylvreInput, string jsRegex)
+        {
+            TranspileOutputBase output = Transpiler.TranspileSylvreToTarget(
+                Parser.ParseSylvreInput(sylvreInput), TargetLanguage.Javascript);
+
+            StringAssert.IsMatch(jsRegex, output.TranspiledCode);
+        }
+
+        [TestCase("function quickSort PARAMS num_array, low, high < >")]
+        [TestCase("function quickSort < >")]
+        [TestCase("function quickSort PARAMS single < >")]
+        public void Should_Not_Provide_Transpile_Errors(string sylvreInput)
+        {
+            TranspileOutputBase output = Transpiler.TranspileSylvreToTarget(
+               Parser.ParseSylvreInput(sylvreInput), TargetLanguage.Javascript);
+
+            Assert.IsFalse(output.HasTranspileErrors);
+        }
+    }
+}
