d2331450f884327cb753a456142cc027acc893b0
Shahzaib Mirani <shahzaib6874@gmail.com>
2019-02-25 23:32:36 +0000

    Update test regex (JS transpiler)

--- src/Sylvre.Tests/Core/TranspilerTests/JavaScript/LoopBlockTests.cs
+++ src/Sylvre.Tests/Core/TranspilerTests/JavaScript/LoopBlockTests.cs
@@ -28,10 +28,10 @@ namespace Sylvre.Tests.Core.TranspilerTests.JavaScript
 
         [TestCase(
             "loopfor(create test=0# i LTHAN test2.length# i increment#) <>",
-           @"""use strict"";[\n ]*for[\n ]*\(.*;.*;\)[\n ]*{[\n ]*}")]
+           @"""use strict"";[\n ]*for[\n ]*\(.*;.*;.*\)[\n ]*{[\n ]*}")]
         [TestCase(
             "loopfor(test=0# i LTHAN test2.length# increment i) <>",
-           @"""use strict"";[\n ]*for[\n ]*\(.*;.*;\)[\n ]*{[\n ]*}")]
+           @"""use strict"";[\n ]*for[\n ]*\(.*;.*;.*\)[\n ]*{[\n ]*}")]
         public void Should_Output_Valid_For_Loop_Block(string sylvreInput, string regexToMatch)
         {
             SylvreProgram program = Parser.ParseSylvreInput(sylvreInput);
