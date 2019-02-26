ae91f5340ad97edf522e5f3c004d329444aad10a
Shahzaib Mirani <shahzaib6874@gmail.com>
2019-02-26 01:00:44 +0000

    Implement conditional expression transpiler (JS)

---
+++ src/Sylvre.Core/Transpilers/JavaScript/ExpressionVisitors.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using Antlr4.Runtime.Misc;
+
+using static SylvreParser;
+
+namespace Sylvre.Core.Transpilers.JavaScript
+{
+    internal partial class JavaScriptTranspiler : SylvreParserBaseVisitor<object>
+    {
+        public override object VisitConditional_expression([NotNull] Conditional_expressionContext context)
+        {
+            if (context.OPEN_PARENTHESIS() != null)
+            {
+                _output.Append('(');
+                VisitConditional_expression(context.conditional_expression()[0]);
+                _output.Append(')');
+            }
+            else if (context.NOT() != null)
+            {
+                _output.Append('!');
+                VisitConditional_expression(context.conditional_expression()[0]);
+            }
+            else if (context.comparison_operator() != null)
+            {
+                VisitConditional_expression(context.conditional_expression()[0]);
+
+                var op = context.comparison_operator();
+                if (op.GREATER_THAN() != null)
+                    _output.Append('>');
+                else if (op.GREATER_EQUAL() != null)
+                    _output.Append(">=");
+                else if (op.LESS_THAN() != null)
+                    _output.Append('<');
+                else if (op.LESS_EQUAL() != null)
+                    _output.Append("<=");
+                else if (op.EQUALS() != null)
+                    _output.Append("==");
+
+                VisitConditional_expression(context.conditional_expression()[1]);
+            }
+            else if (context.logical_operator() != null)
+            {
+                VisitConditional_expression(context.conditional_expression()[0]);
+
+                var op = context.logical_operator();
+                if (op.AND() != null)
+                    _output.Append("&&");
+                else if (op.OR() != null)
+                    _output.Append("||");
+
+                VisitConditional_expression(context.conditional_expression()[1]);
+            }
+            else
+            {
+                VisitExpression(context.expression());
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/Sylvre.Tests/Core/TranspilerTests/JavaScript/ConditionalExpressionTests.cs
@@ -0,0 +1,56 @@
+﻿using Sylvre.Core;
+using Sylvre.Core.Models;
+
+using NUnit.Framework;
+
+namespace Sylvre.Tests.Core.TranspilerTests.JavaScript
+{
+    class ConditionalExpressionTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(
+            "create test = (-20 * -87) EQUALS 1234#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*\(.*\)==.*;")]
+        [TestCase(
+            "create test = (((20 + -87)) LTHAN (testvar2 + 38.02))#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*\(\(.*\(.*\)\).*<.*\(.*\)\)[\n ]*;")]
+        [TestCase(
+            "create test = ((20 + -87) LEQUAL (testvar2 + 38.02))#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*\(\(.*\).*<=.*\(.*\)\)[\n ]*;")]
+        [TestCase(
+            "create test = ((20 + -87)) GTHAN (testvar2 + 38.02)#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*\(.*\(.*\)\).*>.*\(.*\)[\n ]*;")]
+        [TestCase(
+            "create test = ((20 + -87)) GEQUAL (testvar2 + 38.02)#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*\(.*\(.*\)\).*>=.*\(.*\)[\n ]*;")]
+        [TestCase(
+            "create test = NOT var#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*!.*[\n ]*;")]
+        [TestCase(
+            "create test = NOT NOT var#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*!.*!.*[\n ]*;")]
+        [TestCase(
+            "create test = NOT NOT (var)#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*!.*!.*\(.*\)[\n ]*;")]
+        [TestCase(
+            "create test = (-20 * -87) AND (1234 / 23)#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*\(.*\)[\n ]*&&[\n ]*\(.*\)[\n ]*;")]
+        [TestCase(
+            "create test = (-20 + -87) OR (1234 - 23)#",
+           @"""use strict"";[\n ]*var[\n ]+test[\n ]*=[\n ]*\(.*\)[\n ]*||[\n ]*\(.*\)[\n ]*;")]
+        public void Should_Output_Valid_Conditional_Expression(string sylvreInput, string jsRegex)
+        {
+            SylvreProgram program = Parser.ParseSylvreInput(sylvreInput);
+            Assert.IsFalse(program.HasParseErrors);
+
+            TranspileOutputBase output = Transpiler.TranspileSylvreToTarget(
+                program, TargetLanguage.Javascript);
+
+            StringAssert.IsMatch(jsRegex, output.TranspiledCode);
+        }
+    }
+}
