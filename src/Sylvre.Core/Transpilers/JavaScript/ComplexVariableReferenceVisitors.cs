987f9b7a3541f0376ce34528945d2d15f7ff7b1f
Shahzaib Mirani <shahzaib6874@gmail.com>
2019-02-27 02:56:17 +0000

    Implement complex var references transpiler (JS)

---
+++ src/Sylvre.Core/Transpilers/JavaScript/ComplexVariableReferenceVisitors.cs
@@ -0,0 +1,97 @@
+﻿using Antlr4.Runtime.Misc;
+
+using static SylvreParser;
+
+namespace Sylvre.Core.Transpilers.JavaScript
+{
+    internal partial class JavaScriptTranspiler : SylvreParserBaseVisitor<object>
+    {
+        public override object VisitVariable_complex_reference([NotNull] Variable_complex_referenceContext context)
+        {
+            VisitVariable_reference(context.variable_reference());
+
+            if (context.variable_suffix() != null)
+            {
+                foreach (var suffix in context.variable_suffix())
+                {
+                    VisitVariable_suffix(suffix);
+                }
+            }
+
+            return null;
+        }
+        public override object VisitVariable_complex_reference_left([NotNull] Variable_complex_reference_leftContext context)
+        {
+            VisitVariable_reference(context.variable_reference());
+
+            if (context.variable_suffix_left() != null)
+            {
+                foreach (var suffix in context.variable_suffix_left())
+                {
+                    VisitVariable_suffix_left(suffix);
+                }
+            }
+
+            return null;
+        }
+
+        public override object VisitVariable_suffix([NotNull] Variable_suffixContext context)
+        {
+            if (context.member_reference() != null)
+            {
+                VisitMember_reference(context.member_reference());
+            }
+            else if (context.index_reference() != null)
+            {
+                VisitIndex_reference(context.index_reference());
+            }
+
+            return null;
+        }
+        public override object VisitVariable_suffix_left([NotNull] Variable_suffix_leftContext context)
+        {
+            if (context.member_reference_left() != null)
+            {
+                VisitMember_reference_left(context.member_reference_left());
+            }
+            else if (context.index_reference() != null)
+            {
+                VisitIndex_reference(context.index_reference());
+            }
+
+            return null;
+        }
+
+        public override object VisitMember_reference([NotNull] Member_referenceContext context)
+        {
+            _output.Append('.');
+
+            if (context.variable_reference() != null)
+            {
+                VisitVariable_reference(context.variable_reference());
+            }
+            else if (context.function_call() != null)
+            {
+                VisitFunction_call(context.function_call());
+            }
+
+            return null;
+        }
+        public override object VisitMember_reference_left([NotNull] Member_reference_leftContext context)
+        {
+            _output.Append('.');
+            VisitVariable_reference(context.variable_reference());
+
+            return null;
+        }
+
+        public override object VisitIndex_reference([NotNull] Index_referenceContext context)
+        {
+            _output.Append('[');
+            VisitExpression(context.expression());
+            _output.Append(']');
+
+            return null;
+        }
+    }
+}
---
+++ src/Sylvre.Tests/Core/TranspilerTests/JavaScript/ComplexVariableReferenceTests.cs
@@ -0,0 +1,75 @@
+﻿using Sylvre.Core;
+using Sylvre.Core.Models;
+
+using NUnit.Framework;
+
+namespace Sylvre.Tests.Core.TranspilerTests.JavaScript
+{
+    class ComplexVariableReferenceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(
+            "create testvar = Test.var#",
+           @"""use strict"";.*Test\.__var.*;")]
+        [TestCase(
+            "create testvar = Test.value.member#",
+           @"""use strict"";.*Test\.value\.member.*;")]
+        public void Should_Output_Valid_ComplexVarReference_MemberRef(string sylvreInput,
+            string regexToMatch)
+        {
+            SylvreProgram program = Parser.ParseSylvreInput(sylvreInput);
+            Assert.IsFalse(program.HasParseErrors);
+
+            TranspileOutputBase output = Transpiler.TranspileSylvreToTarget(
+                program, TargetLanguage.Javascript);
+            Assert.IsFalse(output.HasTranspileErrors);
+
+            StringAssert.IsMatch(regexToMatch, output.TranspiledCode);
+        }
+
+        [TestCase(
+            "create testvar = call Test.return()#",
+           @"""use strict"";.*Test.*__return.*\(.*\).*;")]
+        [TestCase(
+            "create testvar = call Test.value.someMethod()#",
+           @"""use strict"";.*Test\.value.*someMethod.*\(.*\).*;")]
+        public void Should_Output_Valid_ComplexVarReference_FunctionRef(string sylvreInput,
+            string regexToMatch)
+        {
+            SylvreProgram program = Parser.ParseSylvreInput(sylvreInput);
+            Assert.IsFalse(program.HasParseErrors);
+
+            TranspileOutputBase output = Transpiler.TranspileSylvreToTarget(
+                program, TargetLanguage.Javascript);
+            Assert.IsFalse(output.HasTranspileErrors);
+
+            StringAssert.IsMatch(regexToMatch, output.TranspiledCode);
+        }
+
+        [TestCase(
+            "create testvar = testVar[20]#",
+           @"""use strict"";.*testVar.*\[.*\].*;")]
+        [TestCase(
+            "create testvar = testVar[1][43]#",
+           @"""use strict"";.*testVar.*\[.*\].*\[.*\].*;")]
+        [TestCase(
+            "create testvar = testVar[1][43][i+2]#",
+           @"""use strict"";.*testVar.*\[.*\].*\[.*\].*\[.*\];")]
+        public void Should_Output_Valid_ComplexVarReference_IndexRef(string sylvreInput,
+            string regexToMatch)
+        {
+            SylvreProgram program = Parser.ParseSylvreInput(sylvreInput);
+            Assert.IsFalse(program.HasParseErrors);
+
+            TranspileOutputBase output = Transpiler.TranspileSylvreToTarget(
+                program, TargetLanguage.Javascript);
+            Assert.IsFalse(output.HasTranspileErrors);
+
+            StringAssert.IsMatch(regexToMatch, output.TranspiledCode);
+        }
+    }
+}
