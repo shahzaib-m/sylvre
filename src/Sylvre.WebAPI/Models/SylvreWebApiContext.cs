3ca3841f4732fa2f89e700b48f569ff1bb43fc81
Shahzaib Mirani <shahzaib6874@gmail.com>
2019-03-02 19:02:50 +0000

    Add initial ASP.NET Core WebAPI project

---
+++ src/Sylvre.WebAPI/Models/SylvreWebApiContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sylvre.WebAPI.Models
+{
+    public class SylvreWebApiContext : DbContext
+    {
+        public SylvreWebApiContext(DbContextOptions<SylvreWebApiContext> options) : base(options) { }
+    }
+}
---
+++ src/Sylvre.WebAPI/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Sylvre.WebAPI.Models;
+
+namespace Sylvre.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.${env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddEntityFrameworkNpgsql().AddDbContext<SylvreWebApiContext>(opt =>
+                opt.UseNpgsql(Configuration.GetConnectionString("SylvreWebApiDbConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
